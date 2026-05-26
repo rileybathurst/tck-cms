3c0f2de4de5838d3da8a340b9df2a83287e3fb74
Riley Bathurst <riley@rileybathurst.com>
2026-05-26 20:41:03 +0000

    26 may 26

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -623,6 +623,7 @@ export interface ApiDeliveryDelivery extends Struct.SingleTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    excerpt: Schema.Attribute.String;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
