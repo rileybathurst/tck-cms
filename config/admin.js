17a560f316bfab5eb2b53aa0d9be31681fa9139e
Riley Bathurst <riley@rileybathurst.com>
2026-06-01 22:21:37 +0000

    june 26

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,21 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
- transfer: {
-    token: {
-      salt: env('TRANSFER'),
+module.exports = ({ env }) => {
+
+  return {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+      sessions: {
+        maxSessionLifespan: 1800,
+	maxRefreshTokenLifespan: 2592000,
+      },
     },
-  },
-});
+    apiToken: {
+      salt: env('API_TOKEN_SALT'),
+    },
+    transfer: {
+      token: {
+        salt: env('TRANSFER'),
+      },
+    },
+  };
+};
+
